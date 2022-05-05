3327cea12a760a8f8219b27404c0a48a730ea1c5
Takuya1616 <tky.naga0116@gmail.com>
2022-05-05 04:46:33 +0000

    ADD config.hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+config.hosts << "3a57f3b70e384ceda62d07b315139c0f.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
