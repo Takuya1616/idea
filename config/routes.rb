bb528810ddf8faaf64817c072eeb86480cde8137
Takuya1616 <tky.naga0116@gmail.com>
2022-05-07 15:46:43 +0000

    ADD

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/show'
   root to: "homes#top"
   get "/about"=>'homes#about'
-  get '/users/my_page' => 'users#show'
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions",
+    registrations: 'users/registrations' }
+  resources :users, only:[:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
