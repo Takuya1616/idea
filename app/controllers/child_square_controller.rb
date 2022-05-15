af3a1e1578967f0ad51ca1793da471eb7d5a6431
Takuya1616 <tky.naga0116@gmail.com>
2022-05-15 12:29:42 +0000

    Fix

--- app/controllers/child_square_controller.rb
+++ app/controllers/child_square_controller.rb
@@ -1,5 +1,8 @@
 class ChildSquareController < ApplicationController
+
+  before_action :authenticate_user!
   def show
+    @square = ParentSquare.find(params[:id])
   end
 
   def create
@@ -7,4 +10,15 @@ class ChildSquareController < ApplicationController
 
   def update
   end
+
+   private
+
+  def child_square_params
+    params.require(:child_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9, :position)
+  end
+
+  def parent_square_params
+    params.require(:parent_square).permit(:row_1, :row_2, :row_3, :row_4, :row_6, :row_7, :row_8, :row_9)
+  end
+
 end
