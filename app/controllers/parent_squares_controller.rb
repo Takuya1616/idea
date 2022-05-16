3735271c7e8dcc71eb11e09d5cc244a3a6fff127
Takuya1616 <tky.naga0116@gmail.com>
2022-05-16 15:18:22 +0000

    Fix

--- app/controllers/parent_squares_controller.rb
+++ app/controllers/parent_squares_controller.rb
@@ -3,25 +3,25 @@ class ParentSquaresController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @square = ParentSquare.new
+    @parent_square = ParentSquare.new
   end
 
   def show
-    @square = ParentSquare.find(params[:id])
+    @parent_square = ParentSquare.find(params[:id])
   end
 
 
 
   def create
     if params[:id].present?
-      @square = ParentSquare.find(params[:id])
-      @square.update(parent_square_params)
+      @parent_square = ParentSquare.find(params[:id])
+      @parent_square.update(parent_square_params)
     else
-      @square = ParentSquare.new(parent_square_params)
+      @parent_square = ParentSquare.new(parent_square_params)
     end
-    @square.user_id = current_user.id
-    if @square.save
-      redirect_to parent_square_path(@square.id)
+    @parent_square.user_id = current_user.id
+    if @parent_square.save
+      redirect_to parent_square_path(@parent_square.id)
     else
       render 'index'
     end
@@ -31,9 +31,9 @@ class ParentSquaresController < ApplicationController
     @parent_square = ParentSquare.find(params[:id])
     row_5_value = params[:row_5]
     # @child_square = ChildSquare.new
-    if @parent_square.update(parent_square_params)
-       @child_square = ChildSquare.create(parent_square_id: @parent_square, position: 1,  row_5: row_5_value)
-    end
+    # if @parent_square.update(parent_square_params)
+    #   @child_square = ChildSquare.create(parent_square_id: @parent_square, position: 1,  row_5: row_5_value)
+    # end
   end
 
   def destroy
@@ -44,6 +44,10 @@ class ParentSquaresController < ApplicationController
   def parent_square_params
     params.require(:parent_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9)
   end
+
+  # def child_square_params
+  #   params.require(:child_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9, :position)
+  # end
 
 
 end
