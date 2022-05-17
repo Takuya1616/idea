09761ecd913b861835090c8baf32f6bcf4ef5d11
Takuya1616 <tky.naga0116@gmail.com>
2022-05-17 16:09:39 +0000

    Fix

--- app/controllers/parent_squares_controller.rb
+++ app/controllers/parent_squares_controller.rb
@@ -4,23 +4,24 @@ class ParentSquaresController < ApplicationController
 
   def index
     @parent_square = ParentSquare.new
+    @parent_squares = ParentSquare.all
   end
 
   def show
     @parent_square = ParentSquare.find(params[:id])
+    #子マンダラートを呼び出す
+    @child_squares = @parent_square.child_squares
   end
 
 
 
   def create
-    if params[:id].present?
-      @parent_square = ParentSquare.find(params[:id])
-      @parent_square.update(parent_square_params)
-    else
-      @parent_square = ParentSquare.new(parent_square_params)
-    end
+    @parent_square = ParentSquare.new(parent_square_params)
     @parent_square.user_id = current_user.id
     if @parent_square.save
+      8.times do |child_square|
+        ChildSquare.create(parent_square_id: @parent_square.id)
+      end
       redirect_to parent_square_path(@parent_square.id)
     else
       render 'index'
@@ -28,12 +29,17 @@ class ParentSquaresController < ApplicationController
   end
 
   def update
+    @child_square = ChildSquare.find(params[:id])
     @parent_square = ParentSquare.find(params[:id])
-    row_5_value = params[:row_5]
+    # row_5_value = params[:row_5]
     # @child_square = ChildSquare.new
-    # if @parent_square.update(parent_square_params)
+     if @parent_square.update(parent_square_params)
     #   @child_square = ChildSquare.create(parent_square_id: @parent_square, position: 1,  row_5: row_5_value)
-    # end
+     redirect_to request.referer
+     end
+     if @child_square.update(child_square_params)
+       redirect_to request.referer
+     end
   end
 
   def destroy
@@ -45,9 +51,9 @@ class ParentSquaresController < ApplicationController
     params.require(:parent_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9)
   end
 
-  # def child_square_params
-  #   params.require(:child_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9, :position)
-  # end
+   def child_square_params
+     params.require(:child_square).permit(:row_1, :row_2, :row_3, :row_4, :row_5, :row_6, :row_7, :row_8, :row_9, :position)
+   end
 
 
 end
