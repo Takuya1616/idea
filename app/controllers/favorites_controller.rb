58454eaea77718be3cf26a7c933dc0f7c408f9c8
Takuya1616 <tky.naga0116@gmail.com>
2022-05-24 04:09:58 +0000

    ADD favorite index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,11 @@ class FavoritesController < ApplicationController
 
   before_action :authenticate_user!
 
+  def index
+    favorites = Favorite.where(user_id: current_user.id).pluck(:parent_square_id)
+    @favorite_list = ParentSquare.find(favorites)
+  end
+
   def create
     @parent_square = ParentSquare.find(params[:parent_square_id])
     favorite = current_user.favorites.new(parent_square_id: @parent_square.id)
